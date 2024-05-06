6ccbd6c2cf0cbac543bb96a3afb69f688e151459
Aaron Anderson <aaronanderson@HQ-D4QGX3W0NJ-AaronAnderson-012024-Pro36M3.local>
2024-05-05 20:09:14 -0700

    HW3

---
+++ TargetSignIn_BDD.feature
@@ -0,0 +1,6 @@
+Feature: Test Scenarios for Target Sign In page
+
+  Scenario: Verify Target Sign In
+    Given Open Target page
+    When Click on sign in
+    Then Verify sign in message
---
+++ TargetSignIn_BDD.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+
+
+@given('Open Target page')
+def open_target(context):
+    context.driver.get('https://www.target.com/')
+    sleep(5)
+
+
+@when('Click on sign in')
+def click_cart_icon(context):
+    context.driver.find_element(By.XPATH, value="//span[contains(text(),'Sign in')]").click()
+
+    # Click SignIn from side navigation
+    context.driver.find_element(By.XPATH, value="//a[@data-test='accountNav-signIn']").click()
+    sleep(5)
+
+@then('Verify sign in message')
+def empty_cart_text(context):
+    actual_text = context.driver.find_element(By.XPATH, value="//span[contains(text(),'Sign into your Target account')]")
+
+    # Assert
+    assert 'Sign into your Target account' in actual_text.text, f'Error! Sign into your Target account not in {actual_text}'
+    print("Test case has passed")
+
